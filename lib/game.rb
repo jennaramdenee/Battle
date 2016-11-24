664524c2b31c2ae99d5b97b91ff8d1d611a99853
Agata Sumowska <agata.sumowska@gmail.com>
2016-11-24 14:46:16 +0000

    Adds turn method

--- lib/game.rb
+++ lib/game.rb
@@ -2,15 +2,21 @@ require './lib/player'
 
 class Game
 
-  attr_reader :player1, :player2
+  attr_reader :player1, :player2, :players
 
   def initialize(player1, player2, klass)
     @player1 = klass.new(player1)
     @player2 = klass.new(player2)
+    @players = [@player1, @player2]
   end
 
-  def attack(player)
-    player.receive_attack
+  def attack
+    attacked_player = @players.last
+    attacked_player.receive_attack
+  end
+
+  def turn
+    @players.reverse!
   end
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -4,11 +4,11 @@ describe Game do
 
   subject(:game) { described_class.new(player1, player2, klass) }
   let(:klass) { double(:klass) }
-  let(:player1) { double(:player) }
-  let(:player2) { double(:player) }
+  let(:player1) { double(:player1) }
+  let(:player2) { double(:player2) }
   let(:player) { double(:player) }
 
-  before { allow(klass).to receive(:new).and_return(:player) }
+  before { allow(klass).to receive(:new).and_return(player) }
 
   it "creates two players on initialization" do
     expect(game.player1).not_to be nil
@@ -16,13 +16,21 @@ describe Game do
   end
 
   it "responds to a method called attack with one argument" do
-    expect(game).to respond_to(:attack).with(1).argument
+    expect(game).to respond_to(:attack)
   end
 
   it "can attack a player" do
-    allow(player1).to receive(:receive_attack)
-    expect(player1).to receive(:receive_attack)
-    game.attack(player1)
+    allow(player).to receive(:receive_attack)
+    expect(player).to receive(:receive_attack)
+    game.attack
+  end
+
+  it "stores players in an array" do
+    expect(game.players).to eq [game.player1, game.player2]
+  end
+
+  it "ends a turn" do
+    expect(game.turn).to eq [game.player2, game.player1]
   end
 
 end
