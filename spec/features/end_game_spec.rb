3d127357bcecc826a2dd6f9fe42d87b9a437aa11
Jenna Ramdenee <jenna_ramdenee@hotmail.co.uk>
2016-11-25 14:26:22 +0000

    Start page redesigned

--- spec/features/end_game_spec.rb
+++ spec/features/end_game_spec.rb
@@ -4,22 +4,16 @@ require "spec_helper"
 feature "Ends Game" do
  scenario "Player 2 has run out of life" do
    sign_in_and_play
-   11.times do
-     attack
-     return_to_game
-   end
+   11.times { attack_and_confirm }
    expect(page).to have_content("Jenna loses, Dan wins!")
  end
 
  scenario "Start again when the battle is over" do
    sign_in_and_play
-   11.times do
-     attack
-     return_to_game
-   end
+   11.times { attack_and_confirm }
    start_again
    sign_in_and_play
    expect(page).to have_content("Dan vs Jenna!")
  end
- 
+
 end
--- spec/features/web_helpers.rb
+++ spec/features/web_helpers.rb
@@ -13,6 +13,11 @@ def return_to_game
   click_button "Return To Game"
 end
 
+def attack_and_confirm
+  attack
+  return_to_game
+end
+
 def start_again
   click_button "Start Again"
 end
